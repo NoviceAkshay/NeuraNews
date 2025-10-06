e44d94ed268edf1cdef10201a9d4e88dcfe9da7a
NoviceAkshay <akshayothwor@gmail.com>
2025-10-06 14:55:10 +0530

    adding comment

--- backend/database.py
+++ backend/database.py
@@ -20,7 +20,7 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME")
 
-# ✅ Correct driver prefix
+# Correct driver prefix
 DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 print("DEBUG DATABASE_URL:", DATABASE_URL)
